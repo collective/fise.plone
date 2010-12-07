9a297997d465b129edc3c0b355d6f899ddddba49
Jens W. Klein <jens@bluedynamics.com>
2010-12-07 16:45:05 +0100

    init

---
+++ README.rst
@@ -0,0 +1,7 @@
+This is the `Plone <http://www.plone.org/>`_ integration of 
+`FISE <http://wiki.iks-project.eu/index.php/FISE>`_  (Furtwangen IKS Semantic 
+Engine). 
+
+A good `introduction an overview of FISE features <http://blogs.nuxeo.com/dev/2010/08/introducing-fise-the-restful-semantic-engine.html>`_ 
+has been written by Olivier Grisel. 
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+shortdesc = 'FISE Semantic Engine: RESTful Plone Integration.'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+tests_require = ['interlude']
+
+setup(name='fise.plone',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python', 
+            'Topic :: Software Development',       
+      ],
+      keywords='',
+      author='Jens Klein    ',
+      author_email='jens@bluedynamics.com',
+      url=u'',
+      license='Simplifed BSD like',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+            'setuptools',
+            'fise.client',
+            'Plone',
+
+      ],
+      tests_require=tests_require,
+      test_suite="fise.client.tests.test_suite",
+      extras_require = dict(
+          test=tests_require,
+      ),
+)
